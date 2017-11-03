4782e76c13302ec8abb133f58da7ec2e7537acb1
John Murphy <murf2073@gmail.com>
2017-11-03 17:53:47 -0400

    added data validations for boats and jobs models

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,4 +1,7 @@
 class Job < ApplicationRecord
 	has_many :boat_jobs
 	has_many :boats, through: :boat_jobs
+	validates :description, uniqueness: true 
+	validates :cost, numericality: { greater_than: 1000 }
+	validates :description, length: { minimum: 10 }
 end
